a0c7e35b88012361039db125cdfe06a3c2cf0721
mirnaib <166799804+mirnaib@users.noreply.github.com>
2026-06-14 10:24:42 +0300

    Delete ast directory

--- ast/nodes/Classes/Classes.java
+++
@@ -1,9 +0,0 @@
-package ast.nodes.Classes;
-
-public interface Classes {
-
-    public  String generateCode();
-    public  String symbol();
-    public int getLine();
-    public String getToken();
-}
